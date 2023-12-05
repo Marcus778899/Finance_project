406447377ba3cfc9bf02e0f31ed9e012acec914e
Marcus_Lin <marcus.lin@e-elements.com.tw>
2023-12-05 15:10:12 +0800

    This is first commit use FinMind to scraping stock dataset On branch master Initial commit Changes to be committed: 	new file:   .gitignore 	new file:   poetry.lock 	new file:   pyproject.toml 	new file:   stocke_info.py

---
+++ stocke_info.py
@@ -0,0 +1,9 @@
+from FinMind.data import DataLoader
+from FinMind_token import token
+import pandas as pd
+
+api = DataLoader()
+api.login_by_token(api_token=token)
+
+df = api.taiwan_stock_info()
+df.to_csv('./dataset/stock_info.csv')
